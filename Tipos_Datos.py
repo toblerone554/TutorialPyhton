7c554966983145baf16352110e01e2c9fc2c4962
toblerone554 <toblerone554@gmail.com>
2016-05-03 16:39:22 +0200

    Tipos de datos

---
+++ Tipos_Datos.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#coding: utf-8
+
+# Creamos variable tipo Cadena texto (String)
+
+cadena = "Hola Mundo"
+
+# Creamos una cadena multilinea, usando comillas dobles
+
+cadena_multilinea = """ 
+Vamos a crear
+una variable
+de tipo 
+multilinea
+"""
+
+# Creamos una variable de tipo entero
+
+edad = 30
+
+# Creamos una variable de tipo octal
+
+octal = 030
+
+# Creamos una variable de tipo número entero hexadecimal
+
+hexadecimal = 0X30
+
+# Creamos una variable de tipo número real 
+
+precio = 1234.56
+
+# Creamos una variable de tipo Booleano (True/False)
+
+verdadero = True
+falso = False
